90c488b44852da827de5f8570df2dcbfdf405648
Vladyslav Piskun <vpiskun00@gmail.com>
2025-01-01 23:19:15 +0100

    Added player movement
    Added player spritesheet

--- main.cpp
+++ main.cpp
@@ -3,81 +3,98 @@
 
 int main()
 {
-    //--------------------------------- initialize -------------------------------------
+    //--------------------------------- INITIALIZE -------------------------------------
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(800, 600), "rpg game", sf::Style::Default, settings);
 
-    sf::CircleShape shape(50.0f, 5);
-    shape.setFillColor(sf::Color::Red);
-    shape.setPosition(sf::Vector2f(100, 100));
-    shape.setOutlineThickness(10);
-    shape.setOutlineColor(sf::Color::Green);
-
-    sf::RectangleShape rectangle(sf::Vector2f(200, 1));
-    rectangle.setPosition(sf::Vector2f(300, 100));
-    rectangle.setFillColor(sf::Color::Blue);
-    //rectangle.setorigin(sf::vector2f(rectangle.getsize() / 2.0f));
-    //rectangle.setrotation(45);
-
-    sf::CircleShape octagon(100.f, 8);
-    octagon.setFillColor(sf::Color::Yellow);
-    octagon.setPosition(250, 250);
 
 
-    //--------------------------------- initialize -------------------------------------
+    //--------------------------------- INITIALIZE -------------------------------------
 
+    //--------------------------------- LOAD -------------------------------------
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
+
+    if (playerTexture.loadFromFile("Assets/Player/Textures/spritesheet.png")) 
+    {
+        std::cout << "Player Images Loaded!" << std::endl;
+        playerSprite.setTexture(playerTexture);
+
+        // Int
+        // X, Y, Width, Height
+        int XIndex = 0;
+        int YIndex = 0;
+        playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+        playerSprite.scale(sf::Vector2f(1, 1));
+
+    }
+    else
+    {
+        std::cout << "Player image faild to load!" << std::endl;
+    }
+    //--------------------------------- LOAD -------------------------------------
+
+    //main game loop
     while (window.isOpen())
     {
-        //--------------------------------- update -------------------------------------
+        //--------------------------------- UPDATE -------------------------------------
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
-                //std::cout << "haha! loser!!!" << std::endl;
                 window.close();
-            }
+
+            //if (event.type == sf::Event::KeyPressed)
+            //{
+            //    if (event.key.code == sf::Keyboard::D)
+            //    {
+            //        //0, 0
+            //        sf::Vector2f position = playerSprite.getPosition();
+            //        playerSprite.setPosition(position + sf::Vector2f(10, 0));
+
+            //    }
+            //}
         }
 
-        //--------------------------------- update -------------------------------------
 
-        //--------------------------------- draw -------------------------------------
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(0.1, 0));
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(0, -0.15));
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(0, 0.2));
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            sf::Vector2f position = playerSprite.getPosition();
+            playerSprite.setPosition(position + sf::Vector2f(-0.25, 0));
+        }
+
+        //--------------------------------- UPDATE -------------------------------------
+
+        //--------------------------------- DRAW -------------------------------------
 
         window.clear(sf::Color::Black);
-        window.draw(shape);
-        window.draw(rectangle);
-        window.draw(octagon);
+        window.draw(playerSprite);
         window.display();
 
-        //--------------------------------- draw -------------------------------------
+        //--------------------------------- DRAW -------------------------------------
     }
     return 0;
 }
 
-
-
-//#include <SFML/Graphics.hpp>
-//
-//int main()
-//{
-//    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Green);
-//
-//    sf::Event event;
-//    while (window.isOpen())
-//    {
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//        }
-//
-//        window.clear();
-//        window.draw(shape);
-//        window.display();
-//    }
-//}
 
